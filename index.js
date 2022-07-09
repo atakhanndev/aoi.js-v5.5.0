4eacff43d52e109b8c58428454b39fafdcc16947
AtakhaNN <100962788+atakhanndev@users.noreply.github.com>
2022-07-09 19:25:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const aoijs = require("aoi.js")
 
-const bot = new aoijs.Bot({
-token: "token",
-prefix: "prefix",
-intents: ["GUILDS", "GUILD_MESSAGES"]
-});
-.
+app.get("/",(req,res) => {
+  res.send("AtakhaNN")
+}).listen(8080, () => {
+  console.log("Bot, 7/24 için hazır.")
+})
+
+ const bot = new aoijs.Bot({
+   token: process.env.token,
+   prefix: "$getGlobalUserVar[prefix]",
+   intents: "all"
+ });
 const loader = new aoijs.LoadCommands(bot)
  loader.load(bot.cmd,"./komutlar/")
 
@@ -19,4 +24,15 @@ code: `Pong! $pingms`
 bot.readyCommand({
     channel: "",
     code: `$log[Bot Aktif Edildi! $userTag[$clientID]]`
+})
+
+bot.status({
+  text: "AtakhaNN#9614",
+  type: "PLAYING",
+  time: 12
+})
+
+bot.variables({
+   degisken:"deger",
+   degisken2:"deger"
 })
